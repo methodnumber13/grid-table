aa722810e44c7ea7d30acb04d1dd1600d6273911
afro <afrow0w13@gmail.com>
2020-07-10 00:22:09 +0300

    changed 3d column's name

--- demo/data.tsx
+++ demo/data.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { ColumnProps } from '../src/Components/ITable';
+import { ColumnProps } from '../src/Components';
 
 export interface DataProps {
     name?: string;
     country?: string;
-    victims?: number;
+    number?: number;
 }
 
 export const columns: ColumnProps<DataProps>[] = [
@@ -21,24 +21,24 @@ export const columns: ColumnProps<DataProps>[] = [
         toRender: ({ record }) => <span className='render_column_wrapper'>{record.country}</span>,
     },
     {
-        title: 'victims number',
-        key: 'victims',
-        dataIndex: 'victims',
-        toRender: ({ record }) => <span className='render_column_wrapper'>{record.victims}</span>,
+        title: 'Number',
+        key: 'number',
+        dataIndex: 'number',
+        toRender: ({ record }) => <span className='render_column_wrapper'>{record.number}</span>,
     },
 ];
 
 export const data: DataProps[] = [
-    { name: 'Luis Garavito', country: 'Colombia', victims: 300 },
-    { name: 'Pedro López', country: 'Colombia', victims: 300 },
-    { name: 'Javed Iqbal', country: 'Pakistan', victims: 100 },
-    { name: 'Mikhail Popkov', country: 'Russia', victims: 81 },
-    { name: 'Daniel Camargo Barbosa', country: 'Ecuador', victims: 180 },
-    { name: 'Pedro Rodrigues Filho', country: 'Brazil', victims: 100 },
-    { name: 'Kampatimar Shankariya', country: 'India', victims: 70 },
-    { name: 'Yang Xinhai', country: 'China', victims: 67 },
-    { name: 'Andrei Chikatilo', country: 'Soviet Union', victims: 56 },
-    { name: 'Anatoly Onoprienko', country: 'Ukraine', victims: 52 },
-    { name: 'Samuel Little', country: 'USA', victims: 93 },
-    { name: 'Florisvaldo de Oliveira', country: 'Brazil', victims: 50 },
+    { name: 'Luis Garavito', country: 'Colombia', number: 300 },
+    { name: 'Pedro López', country: 'Colombia', number: 300 },
+    { name: 'Javed Iqbal', country: 'Pakistan', number: 100 },
+    { name: 'Mikhail Popkov', country: 'Russia', number: 81 },
+    { name: 'Daniel Camargo Barbosa', country: 'Ecuador', number: 180 },
+    { name: 'Pedro Rodrigues Filho', country: 'Brazil', number: 100 },
+    { name: 'Kampatimar Shankariya', country: 'India', number: 70 },
+    { name: 'Yang Xinhai', country: 'China', number: 67 },
+    { name: 'Andrei Chikatilo', country: 'Soviet Union', number: 56 },
+    { name: 'Anatoly Onoprienko', country: 'Ukraine', number: 52 },
+    { name: 'Samuel Little', country: 'USA', number: 93 },
+    { name: 'Florisvaldo de Oliveira', country: 'Brazil', number: 50 },
 ];
